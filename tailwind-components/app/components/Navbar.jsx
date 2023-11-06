5ae46b9244f654da49ef3449ced35b058af8f229
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-11-06 12:21:27 +0200

    Layout init

---
+++ tailwind-components/app/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <header className="w-full mt-5 mb-10">
+      <nav className="flex justify-between py-5 dark:text-white">
+        <div className="logo">Logo</div>
+        <ul className="flex gap-5">
+          <li>
+            <Link href={"/"}>Buttons</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Forms</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Lists</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Sliders</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Toggles</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
